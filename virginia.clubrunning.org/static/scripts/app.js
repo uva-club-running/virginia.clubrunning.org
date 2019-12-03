fffb0735f55feb00d95352f06a6b253612a60e49
Joe Kerrigan <joek1301@gmail.com>
2019-12-02 22:01:01 -0500

    Add a little surprise for Andrew

--- virginia.clubrunning.org/static/scripts/app.js
+++ virginia.clubrunning.org/static/scripts/app.js
@@ -7,8 +7,6 @@ $(document).on("click", ".contains-expandable > h4" , function(){
 	$(this).parents(".contains-expandable").toggleClass("collapsed");
 });
 
-let anthemLink = "<a href='https://soundcloud.com/user-870446752/sets/club-anthems' id='anthem-link' target='_blank'>now with anthems!</a>";
-$("#header > .site-title > .container").append(anthemLink);
-
 console.log("Andrew McCullough is a god.");
-console.log("Skyler Moon is a thot.");
+console.log("%cSkyler Moon is a thot.", "text-decoration: line-through");
+console.log("Congratulations! If you're reading this, you've found and won the secret Club Running sweepstakes. The first person to email asm2wm@virginia.edu with the phrase \"mystic apples\" in the subject line will receive $100 and a free Club Running shirt. Better act quick!");
