cf09a0457c7533108845071410265ec56f589f8f
Joe Kerrigan <joek1301@gmail.com>
2019-12-03 00:00:04 -0500

    Add script for compiling scss files

---
+++ scripts/sass.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# A script that automatically regenerates CSS from the SCSS files in virginia.clubrunning.org/static/styles, relative to the current working directory.
+
+sass virginia.clubrunning.org/static/styles
