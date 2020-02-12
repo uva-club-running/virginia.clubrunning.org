59059562816f9af23750c9df6109e91abf1c199d
Joe Kerrigan <joek1301@gmail.com>
2020-02-11 23:30:24 -0500

    Some final tweaks to the runathon sign up

--- virginia.clubrunning.org/static/scripts/app.js
+++ virginia.clubrunning.org/static/scripts/app.js
@@ -9,4 +9,4 @@ $(document).on("click", ".contains-expandable > h4" , function(){
 
 console.log("Andrew McCullough is a god.");
 console.log("%cSkyler Moon is a thot.", "text-decoration: line-through");
-console.log("Congratulations! If you're reading this, you've found and won the secret Club Running sweepstakes. The first person to email asm2wm@virginia.edu with the phrase \"mystic apples\" in the subject line will receive $100 and a free Club Running shirt. Better act quick!");
+console.log("Congratulations! If you're reading this, you've found and won the secret Club Running sweepstakes. The first person to email asm4wm@virginia.edu with the phrase \"mystic apples\" in the subject line will receive $100 and a free Club Running shirt. Better act quick!");
