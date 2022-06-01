7329996b7310a2d48bbc27b8ee0637bce9a6fe72
Joe Kerrigan <joek1301@gmail.com>
2022-05-31 21:34:15 -0700

    Sneaky message

--- virginia.clubrunning.org/static/scripts/app.js
+++ virginia.clubrunning.org/static/scripts/app.js
@@ -10,3 +10,7 @@ $(document).on("click", ".contains-expandable > h4" , function(){
 console.log("Andrew McCullough is a god.");
 console.log("%cSkyler Moon is a thot.", "text-decoration: line-through");
 console.log("Congratulations! If you're reading this, you've found and won the secret Club Running sweepstakes. The first person to email asm4wm@virginia.edu with the phrase \"mystic apples\" in the subject line will receive $100 and a free Club Running shirt. Better act quick!");
+
+console.log("Joe Kerrigan here.")
+console.log("I have a confession to make.")
+console.log("I don't even like running. I literally just joined Club Running because I hate vaping.")
